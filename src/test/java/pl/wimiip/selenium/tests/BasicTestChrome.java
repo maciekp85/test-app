d8760787547affea02c8c9d8116e2099491e3687
Maciej Piotrowski <nishi@wimiip.pl>
2016-05-24 19:21:17 +0200

    Prepared two examplary files to run selenium on Chrome and Firefox browsers

---
+++ src/test/java/pl/wimiip/selenium/tests/BasicTestChrome.java
@@ -0,0 +1,41 @@
+package pl.wimiip.selenium.tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import pl.wimiip.TestApp;
+
+/**
+ * Created by nishi on 2016-05-02.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TestApp.class)
+@WebAppConfiguration
+public class BasicTestChrome {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\tools\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void test() throws InterruptedException {
+        driver.get("http://localhost:8080/#/");
+        Thread.sleep(10000);
+    }
+
+    @After
+    public void tearDown() {
+        driver.close();
+    }
+}
---
+++ src/test/java/pl/wimiip/selenium/tests/BasicTestFirefox.java
@@ -0,0 +1,31 @@
+package pl.wimiip.selenium.tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by nishi on 2016-05-08.
+ */
+public class BasicTestFirefox {
+
+    private FirefoxDriver driver;
+
+    @Before
+    public void setUp() {
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void testFirefoxBrowser() throws InterruptedException {
+        driver.get("http://localhost:8080/#/");
+        Thread.sleep(10000);
+    }
+
+    @After
+    public void tearDown() {
+        driver.close();
+    }
+}
