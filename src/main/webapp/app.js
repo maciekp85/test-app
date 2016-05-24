7b88f15116b964982127cf8ae349a2a5135bc66b
Maciej Piotrowski <nishi@wimiip.pl>
2016-05-24 19:14:00 +0200

    Applied Simple Sidebar created by David Miller, which is based on the BootStrap framework created by Mark Otto

---
+++ src/main/webapp/app.js
@@ -0,0 +1,98 @@
+/**
+ * Created by nishi on 2016-05-21.
+ */
+angular.module('app', ['ui.router']);
+
+angular.module('app')
+    .config(['$stateProvider', '$urlRouterProvider', '$httpProvider', function ($stateProvider, $urlRouterProvider, $httpProvider) {
+
+        $urlRouterProvider.otherwise('home');
+
+        $stateProvider
+            // HOME
+            .state('home', {
+                url: '/home',
+                templateUrl: 'home.html',
+                controller: 'home'
+            })
+                .state('home.list', {
+                    url: '/list',
+
+                    template: '<ul id="home-list.html"><li ng-repeat="n in names">{{n}}</li></ul>',
+                    controller: function ($scope) {
+                        $scope.names = ['Ania', 'Ola', 'Ewa'];
+                    }
+                })
+                .state('home.text', {
+                    url: '/text',
+                    template: 'Examplary text!'
+                })
+            // LOGIN
+            .state('login', {
+                url: '/login',
+                views: {
+                    '': {
+                        templateUrl: 'views/login.html'
+                    },
+                    'columnOne@login': {
+                        template: 'First column!'
+                    },
+                    'columnTwo@login': {
+                        template: 'Second column!'
+                    }
+                },
+                controller: 'navigation'
+            })
+
+           $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+    }])
+    .controller('navigation',['$rootScope','$scope','$http', '$location', function($rootScope, $scope, $http, $location) {
+
+        var authenticate = function(credentials, callback) {
+
+            if(typeof callback != "function") {
+                callback = false;
+            }
+
+            var headers = credentials ? { authorization : "Basic "
+            + btoa(credentials.username + ":" + credentials.password)
+            } : {};
+
+            $http.get('user', {headers : headers}).success(function(data) {
+                if (data.name) {
+                    $rootScope.authenticated = true;
+                } else {
+                    $rootScope.authenticated = false;
+                }
+                callback && callback();
+            }).error(function() {
+                $rootScope.authenticated = false;
+                callback && callback();
+            });
+        };
+
+        authenticate();
+        $scope.credentials = {};
+
+        $scope.login = function() {
+            authenticate($scope.credentials, function() {
+                if ($rootScope.authenticated) {
+                    $location.path("/home");
+                    $scope.error = false;
+                } else {
+                    $location.path("/login");
+                    $scope.error = true;
+                }
+            });
+        };
+        $scope.logout = function() {
+            $http.post('logout', {}).success(function() {
+                $rootScope.authenticated = false;
+                $location.path("/home");
+            }).error(function() {
+                $rootScope.authenticated = false;
+                $location.path("/home");
+            });
+        };
+    }]);
+   
