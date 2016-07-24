231783ef1fcb1815fc1f47b54fd9f7aa6b202870
Maciej Piotrowski <nishi@wimiip.pl>
2016-07-24 18:12:20 +0200

    Selenium: 1.2 Created ITLocatingElementsTests (by id)
    [Html]
    - added inputs and button to locating elements by id view
    
    [Selenium]
    + new interface test for locating elements on the page by ID
    + small fixes into readme and other files

--- src/test/java/pl/wimiip/interfaceTests/tests/CommonMethods.java
+++ src/test/java/pl/wimiip/interfaceTests/tests/CommonMethods.java
@@ -11,21 +11,20 @@ public class CommonMethods extends ITConfigurationForChromeBrowser {
 
     public void logIn() {
         driver.get("http://localhost:8080/#/");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Login")));
-        driver.findElement(By.linkText("Login")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Login"))).click();
         fillInForm();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Selenium"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("button"))).click();
     }
 
     private void fillInForm() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("login")));
-        driver.findElement(By.name("login")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("login"))).click();
         driver.findElement(By.name("login")).sendKeys("maciek");
         driver.findElement(By.name("password")).click();
         driver.findElement(By.name("password")).sendKeys("test");
         driver.findElement(By.name("email")).click();
         driver.findElement(By.name("email")).sendKeys("maciek@test.pl");
-        wait.until(ExpectedConditions.elementToBeClickable(By.tagName("button")));
-        driver.findElement(By.tagName("button")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.tagName("button"))).click();
         wait.until(ExpectedConditions.urlContains("home"));
     }
 
--- src/test/java/pl/wimiip/interfaceTests/tests/locating/ITLocatingElementsTest.java
+++ src/test/java/pl/wimiip/interfaceTests/tests/locating/ITLocatingElementsTest.java
@@ -1,14 +1,18 @@
 package pl.wimiip.interfaceTests.tests.locating;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import pl.wimiip.TestApp;
 import pl.wimiip.interfaceTests.config.ITConfigurationForChromeBrowser;
 import pl.wimiip.interfaceTests.tests.CommonMethods;
+import static org.junit.Assert.*;
 
 /**
  * Created by nishi on 2016-06-19.
@@ -29,7 +33,33 @@ public class ITLocatingElementsTest extends ITConfigurationForChromeBrowser {
 
     @Test
     public void testLocatingElementById() {
-
+        assertTrue(wait.until(ExpectedConditions.urlContains("locating")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.partialLinkText("1.2"))).click();
+        fillInForm("Maciek", "test");
+        clearForm();
     }
 
+    @After
+    public void tearDown() {
+        System.out.println("Cleaning after " + ITLocatingElementsTest.class.getSimpleName());
+    }
+
+    private void fillInForm(String login, String password) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("login"))).sendKeys(login);
+        driver.findElement(By.id("password")).sendKeys(password);
+        assertNotNull(driver.findElement(By.id("clickMeButton")));
+        driver.findElement(By.id("clickMeButton")).click();
+    }
+
+    private void clearForm() {
+        if(wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("login"))).isDisplayed()
+            && wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("login"))).getText() != null) {
+            driver.findElement(By.id("login")).clear();
+            assertEquals("", driver.findElement(By.id("login")).getText());
+        }
+        if(wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("password"))).isDisplayed()
+                && wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("password"))).getText() != null)
+            driver.findElement(By.id("password")).clear();
+            assertEquals("", driver.findElement(By.id("login")).getText());
+        }
 }
