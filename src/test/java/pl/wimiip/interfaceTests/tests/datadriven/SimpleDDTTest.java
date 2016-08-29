f6fb5b3764030eba7fbadafd288828cbf977a5ea
Maciej Piotrowski <nishi@wimiip.pl>
2016-08-29 18:57:38 +0200

    Selenium: 4.2 Creating a data-driven test using JUnit (ver. 1)
    [Html]
    + added BMI Calculator application iframe to view
    
    [Selenium/JUnit]
    + create data-driven test using JUnit to test BMI calculator application
    + specify the test data within our JUnit test case class

---
+++ src/test/java/pl/wimiip/interfaceTests/tests/datadriven/SimpleDDTTest.java
@@ -0,0 +1,122 @@
+package pl.wimiip.interfaceTests.tests.datadriven;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import pl.wimiip.interfaceTests.config.ITConfigurationForChromeBrowser;
+import pl.wimiip.interfaceTests.tests.CommonMethods;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by nishi on 2016-08-29.
+ */
+
+// Use a parameterized runner
+@RunWith(value = Parameterized.class)
+public class SimpleDDTTest extends ITConfigurationForChromeBrowser {
+
+    private CommonMethods commonMethods;
+
+    @Rule
+    public TestName name = new TestName();
+
+    // Declare instance variables for the parameterized values
+    private String weight;
+    private String heightMeter;
+    private String heightCm;
+    private String bmi;
+    private String bmiCategory;
+
+    // Define a method that will return the collection of parameters by using the @Parameters annotation
+    @Parameters
+    public static Collection testData() {
+        return Arrays.asList(
+                new Object[][] {
+                        {"45", "1", "60", "17.6", "Underweight"}
+//                        {"168","1", "70", "24.8", "Normal"},
+//                        {"181", "1", "89" "27.2", "Overweight"},
+//                        {"178","1", "00", "31.6", "Obesity"}
+                    }
+                );
+    }
+
+    // Add a constructor which will be used by the test runner to pass the parameters to the SimpleDDT class instance
+    public SimpleDDTTest(String weight, String heightMeter, String heightCm, String bmi, String bmiCategory)
+    {
+        this.weight = weight;
+        this.heightMeter = heightMeter;
+        this.heightCm = heightCm;
+        this.bmi = bmi;
+        this.bmiCategory = bmiCategory;
+    }
+
+    @Before
+    public void setUp() {
+        System.out.println("Starting " + name.getMethodName());
+        commonMethods = new CommonMethods();
+        commonMethods.logInAndMoveToSeleniumPage(driver, wait);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[text()='4. Data-driven Testing']"))).click();
+    }
+
+
+    // Add the test case method testBMICalculator() that uses parameterized variables
+    @Test
+    public void testBMICalculator() throws Exception {
+
+        // Move to proper view
+        commonMethods.moveToExample("datadriven", "4.2");
+
+        // Activate the BMI Calculator iframe
+        driver.switchTo().frame("bmiCal");
+
+        // Get the Weight element and set the value using parameterized weight variable
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Metric - kg/cm"))).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='tabStrip-2']/div[1]/span/span/input[1]"))).click();
+
+        WebElement weightField = driver.findElement(By.xpath("//*[@id='nTWeightKg']"));
+        weightField.clear();
+        weightField.sendKeys(weight);
+
+        // Get the Height element (meter field) and set the value using parameterized height variable
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='tabStrip-2']/div[2]/span/span/input[1]"))).click();
+
+        WebElement heightFieldMeter = driver.findElement(By.xpath("//*[@id='nHM']"));
+        heightFieldMeter.clear();
+        heightFieldMeter.sendKeys(heightMeter);
+
+        // Get the Height element (centimeter field) and set the value using parameterized height variable
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='tabStrip-2']/div[2]/span[2]/span/input[1]"))).click();
+
+        WebElement heightFieldCentimeter = driver.findElement(By.xpath("//*[@id='nHCm']"));
+        heightFieldCentimeter.clear();
+        heightFieldCentimeter.sendKeys(heightCm);
+
+        // Click on Calculate Button
+        WebElement calculateButton = driver.findElement(By.id("calcButtonKg"));
+        calculateButton.click();
+
+        // ...
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Cleaning after " + name.getMethodName());
+    }
+
+
+
+
+}
